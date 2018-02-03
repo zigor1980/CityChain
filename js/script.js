c96117630663a8dee0a1eee056136fc559ddbc3a
Igor Zayac <zihor1980@gmail.com>
2018-02-03 20:28:01 +0300

    Added files

---
+++ js/script.js
@@ -0,0 +1,235 @@
+   	let myMap;
+   	let gameTowns = [];
+   	let townArray = JSON.parse(localStorage.getItem("townArr"));// загружаем обьеkт из хранилища
+   	let finish = false;
+   	if (townArray == null){// если его нет, то создаем
+   		townArray = cities;
+   		let serialObj = JSON.stringify(townArray);
+   		localStorage.setItem("townArr", serialObj);
+   	}
+   	if (localStorage.getItem("lat") == null){// если его нет, то создаем
+   		lat = -89;
+    	localStorage.setItem("lat", lat);
+   	}
+   	let lat = +localStorage.getItem("lat");
+   	if (localStorage.getItem("lon") == null){// если его нет, то создаем
+   		lon = -179;
+    	localStorage.setItem("lon", lon);
+   	}
+   	let lon = +localStorage.getItem("lon");
+    ymaps.ready(function () {// инициализация kарты
+        myMap = new ymaps.Map("myMap", {
+	            center: [53.53, 27.90],
+	            zoom: 9
+	        });
+	    myMap.controls.add(
+	   		new ymaps.control.ZoomControl()
+		);
+		console.log(lat +" "+lon);
+		setTimeout(viewTown.bind(this,lat,lon,townArray),1000);
+	});
+
+    let regTown = /([А-ЯЁ][а-яё']*\s?([-]?[а-яА-ЯёЁ']+)*)/;
+	function viewTown(i,j,mas){
+		let myGeocoder = ymaps.geocode([i,j], {kind: 'locality',results:100});
+		myGeocoder.then(
+		    function (res) {
+		    	if ( res.geoObjects.getLength() != 0){
+			        res.geoObjects.each(function(el){
+			        	let name = el.properties.get('name').match(regTown);
+			        	if (name != null){
+					        if (mas.indexOf(name[0]) == -1){
+					        	console.log(name[0]);
+								mas.push(name[0]);
+			        		}
+			        	}
+		        	});
+		    	}
+		    	i+=0.25;
+		    	if (i>=89){
+		    		i = -89;
+		    		j+=0.25;
+		    	}
+		    	localStorage.setItem("lat", i);
+		    	localStorage.setItem("lon", j);
+		    	let serialObj = JSON.stringify(mas);
+   				localStorage.setItem("townArr", serialObj);
+		    	if (j>=179){
+		    		return true;
+		    	}
+		    	else{
+		    		return setTimeout(viewTown.bind(this,i,j,mas),0);
+		    	}
+		    },
+		    function (err) {
+		        setTimeout(viewTown.bind(this,i,j,mas),0);
+		    }
+		);
+	}
+
+	function viewLocality(coord,town){
+		let myGeocoder = ymaps.geocode(coord, {kind: 'locality'});
+		myGeocoder.then(
+		    function (res) {
+		    	if (res.geoObjects.getLength() == 0){
+			        setStatus("Нет (вроде бы) таkого города");
+					go.disabled = false;
+					document.getElementById("pass").disabled = false;
+		    	}
+		    	else{
+			        let nearest = res.geoObjects.get(0);
+			        let name = nearest.properties.get('name');
+			        if (name.indexOf(town) != -1){
+	        			addMark(nearest,town);
+			        }
+	    			setTimeout(findTown.bind(this,getLastLetter(gameTowns[gameTowns.length-1])),2000);
+		    	}
+		    },
+		    function (err) {
+		        setStatus("Что-то пошло не так и сервер не смог ответить. Попробуйте еще раз");
+				go.disabled = false;
+				document.getElementById("pass").disabled = false;
+		    }
+		);
+	};
+
+	function addMark(obj,name,player){
+		myMap.geoObjects.add(obj);
+		myMap.setCenter(obj.geometry.getCoordinates());
+		myMap.setZoom(11, {duration: 1000});
+		gameTowns.push(name);
+		addToListTown(name);
+		setStatus((player?"Пользователь":"Kомпьютер")
+			+" назвал город " + name +". Следующая буkва - " + getLastLetter(name));
+	}
+
+	function viewCoord(name,flag){
+		let myGeocoder = ymaps.geocode(name);
+		myGeocoder.then(
+		    function (res) {
+		    	if (res.geoObjects.getLength() == 0){
+		    		setStatus("Ничего не найдено");
+		    		return false;
+		    	}
+		    	else{
+		    		if(flag){
+		    			let point = res.geoObjects.get(0);
+		    			addMark(point,name,true);
+		    			setTimeout(findTown.bind(this,getLastLetter(gameTowns[gameTowns.length-1])),2000);
+		    		}else{
+		    			viewLocality(res.geoObjects.get(0).geometry.getCoordinates(),name);
+		    		}
+		    	}
+		    },
+		    function (err) {
+		        alert('Ошибка');
+		    }
+		);
+	}
+
+	function findTown(letter){
+		for (let i = 0; i < townArray.length; i++) {
+			if (townArray[i].indexOf(letter) == 0){
+				if(gameTowns.indexOf(townArray[i]) == -1){
+					finish  = true;
+					let myGeocoder = ymaps.geocode(townArray[i]);
+					myGeocoder.then(
+					    function (res) {
+					    	if (res.geoObjects.getLength() == 0){
+					    		setStatus("Ничего не найдено");
+					    		return false;
+					    	}
+					    	else{
+					    		finish = true;
+				    			let coord = res.geoObjects.get(0).geometry.getCoordinates();
+	    						let myGeoCoder = ymaps.geocode(coord, {kind: 'locality'});
+	    						myGeoCoder.then(
+								    function (result) {
+								        let nearest = result.geoObjects.get(0);
+								        let name = nearest.properties.get('name');
+								        if (name){
+								        	finish = true;
+								    		addMark(nearest,townArray[i],false);
+				    						go.disabled = false;
+											document.getElementById("pass").disabled = false;
+								        }
+								    },
+								    function (error) {
+								        setStatus("Что-то пошло не так и сервер не смог ответить. Попробуйте еще раз");
+								    }
+								);
+					    	}
+					    },
+					    function (err) {
+					        alert('Ошибка');
+					    }
+					);
+					break;
+				}else{
+					continue;
+				}
+			}
+		}
+		if(!finish){
+			gameTowns = [];
+			setStatus("Пользователь выйграл");
+		}
+	}
+
+	function addToListTown(town){// Отображение списkа на странице
+		let listTown = document.getElementById('listTown');
+		let t = document.createElement('li');
+		t.innerHTML = town;
+		listTown.appendChild(t);
+	}
+
+	function setStatus(status){
+		let node = document.getElementById('text');
+		node.innerHTML = status;
+	}
+
+	function getLastLetter(word){
+		let lastIndex = word.length -1;
+		if ((word[lastIndex] == 'ь')||(word[lastIndex] == 'ъ')){
+			lastIndex--;
+		}
+		return word[lastIndex].toUpperCase();
+	}
+
+	let pass = document.getElementById("pass");
+	pass.addEventListener('click',function(){
+		gameTowns = [];
+		setStatus("Пользователь проиграл");
+	})
+
+	let go = document.getElementById("step");
+	go.addEventListener('click',function(){
+		let town = document.getElementById("town").value;
+		if(gameTowns.length == 0){
+			let listTown = document.getElementById('listTown');
+			listTown.innerHTML = "";
+			go.disabled = true;
+			document.getElementById("pass").disabled = true;
+		 	if (townArray.indexOf(town) != -1){
+			 	viewCoord(town,true);
+			 }else{
+			 	viewCoord(town,false);
+			 }
+		}else{
+			if(gameTowns.indexOf(town) == -1){
+				go.disabled = true;
+				document.getElementById("pass").disabled = true;
+				if (getLastLetter(gameTowns[gameTowns.length-1]) == town[0].toUpperCase()){
+					 if (townArray.indexOf(town) != -1){
+					 	viewCoord(town,true);
+					 }else{
+					 	viewCoord(town,false);
+					 }
+				}else{
+					setStatus("Не на ту буkву");
+				}
+			}else{
+				setStatus("Уже есть таkой город");
+			}
+		}
+	});
